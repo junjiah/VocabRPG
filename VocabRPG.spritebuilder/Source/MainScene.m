c052ee8145e9f29eaab8296c12543511ee8f4651
EDFward <edfward@gmail.com>
2015-02-20 17:12:28 -0500

    Automatically Committed for MakeGamesWithUs

--- VocabRPG.spritebuilder/Source/MainScene.m
+++ VocabRPG.spritebuilder/Source/MainScene.m
@@ -15,6 +15,8 @@
 - (id)initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
 @end
 
+static int const BLOCK_X_MARGIN = 77;
+
 @implementation MainScene {
   NSTimeInterval _sinceTouch;
 
@@ -23,14 +25,41 @@
   BOOL _gameOver;
   CCLabelTTF *_nameLabel;
 
-  int points;
+  int _points;
+
+  NSMutableArray *_leftBlocks, *_rightBlocks;
+
+  int _blockSize;
 }
 
 - (void)initialize {
   _character = (Character *)[CCBReader load:@"Character"];
-//  [_physicsNode addChild:_character];
+  //  [_physicsNode addChild:_character];
   [self addObstacle];
   _timeSinceObstacle = 0.0f;
+
+  // init blocks
+  _leftBlocks = [NSMutableArray arrayWithCapacity:4];
+  _rightBlocks = [NSMutableArray arrayWithCapacity:4];
+  _blockSize = 4;
+
+  int block_yspacing = 40, block_ystart = 80;
+
+  for (int i = 0; i < _blockSize; ++i) {
+    LeftBlock *left = (LeftBlock *)[CCBReader load:@"LeftBlock"];
+    left.position = ccp(BLOCK_X_MARGIN, block_ystart + i * block_yspacing);
+    [_leftBlocks addObject:left];
+    [self addChild:left];
+
+    RightBlock *right = (RightBlock *)[CCBReader load:@"RightBlock"];
+    static CCPositionType rightCornerRef = {
+        CCPositionUnitPoints, CCPositionUnitPoints,
+        CCPositionReferenceCornerBottomLeft};
+    right.positionType = rightCornerRef;
+    right.position = ccp(BLOCK_X_MARGIN, block_ystart + i * block_yspacing);
+    [_rightBlocks addObject:right];
+    [self addChild:right];
+  }
 }
 
 - (void)didLoadFromCCB {
@@ -39,7 +68,7 @@
   // set this class as delegate
   _physicsNode.collisionDelegate = self;
 
-  points = 0;
+  _points = 0;
 
   [self initialize];
 }
@@ -47,16 +76,16 @@
 #pragma mark - Touch Handling
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-//  if (!_gameOver) {
-//    [_character.physicsBody applyAngularImpulse:10000.f];
-//    _sinceTouch = 0.f;
-//
-//    @try {
-//      [_character flap];
-//    }
-//    @catch (NSException *ex) {
-//    }
-//  }
+  //  if (!_gameOver) {
+  //    [_character.physicsBody applyAngularImpulse:10000.f];
+  //    _sinceTouch = 0.f;
+  //
+  //    @try {
+  //      [_character flap];
+  //    }
+  //    @catch (NSException *ex) {
+  //    }
+  //  }
 }
 
 #pragma mark - Game Actions
@@ -92,46 +121,48 @@
 #pragma mark - Obstacle Spawning
 
 - (void)addObstacle {
-//  Obstacle *obstacle = (Obstacle *)[CCBReader load:@"Obstacle"];
-//  CGPoint screenPosition = [self convertToWorldSpace:ccp(380, 0)];
-//  CGPoint worldPosition = [_physicsNode convertToNodeSpace:screenPosition];
-//  obstacle.position = worldPosition;
-//  [obstacle setupRandomPosition];
-////  obstacle.zOrder = DrawingOrderPipes;
-//  [_physicsNode addChild:obstacle];
+  //  Obstacle *obstacle = (Obstacle *)[CCBReader load:@"Obstacle"];
+  //  CGPoint screenPosition = [self convertToWorldSpace:ccp(380, 0)];
+  //  CGPoint worldPosition = [_physicsNode convertToNodeSpace:screenPosition];
+  //  obstacle.position = worldPosition;
+  //  [obstacle setupRandomPosition];
+  ////  obstacle.zOrder = DrawingOrderPipes;
+  //  [_physicsNode addChild:obstacle];
 }
 
 #pragma mark - Update
 
 - (void)update:(CCTime)delta {
-//  _sinceTouch += delta;
-//
-//  _character.rotation = clampf(_character.rotation, -30.f, 90.f);
-//
-//  if (_character.physicsBody.allowsRotation) {
-//    float angularVelocity =
-//        clampf(_character.physicsBody.angularVelocity, -2.f, 1.f);
-//    _character.physicsBody.angularVelocity = angularVelocity;
-//  }
-//
-//  if ((_sinceTouch > 0.5f)) {
-//    [_character.physicsBody applyAngularImpulse:-40000.f * delta];
-//  }
-//
-//  _physicsNode.position =
-//      ccp(_physicsNode.position.x - (_character.physicsBody.velocity.x * delta),
-//          _physicsNode.position.y);
-//
-//  if (!_gameOver) {
-//    @try {
-//      _character.physicsBody.velocity = ccp(
-//          80.f, clampf(_character.physicsBody.velocity.y, -MAXFLOAT, 200.f));
-//
-//      [super update:delta];
-//    }
-//    @catch (NSException *ex) {
-//    }
-//  }
+  //  _sinceTouch += delta;
+  //
+  //  _character.rotation = clampf(_character.rotation, -30.f, 90.f);
+  //
+  //  if (_character.physicsBody.allowsRotation) {
+  //    float angularVelocity =
+  //        clampf(_character.physicsBody.angularVelocity, -2.f, 1.f);
+  //    _character.physicsBody.angularVelocity = angularVelocity;
+  //  }
+  //
+  //  if ((_sinceTouch > 0.5f)) {
+  //    [_character.physicsBody applyAngularImpulse:-40000.f * delta];
+  //  }
+  //
+  //  _physicsNode.position =
+  //      ccp(_physicsNode.position.x - (_character.physicsBody.velocity.x *
+  //      delta),
+  //          _physicsNode.position.y);
+  //
+  //  if (!_gameOver) {
+  //    @try {
+  //      _character.physicsBody.velocity = ccp(
+  //          80.f, clampf(_character.physicsBody.velocity.y, -MAXFLOAT,
+  //          200.f));
+  //
+  //      [super update:delta];
+  //    }
+  //    @catch (NSException *ex) {
+  //    }
+  //  }
 }
 
 //- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair
@@ -145,7 +176,7 @@
 //                      character:(CCNode *)character
 //                           goal:(CCNode *)goal {
 //  [goal removeFromParent];
-//  points++;
+//  _points++;
 //  return TRUE;
 //}
 
