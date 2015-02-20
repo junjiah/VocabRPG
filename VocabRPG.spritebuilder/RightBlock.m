d105deabcf3d77bd19587b37cfcf49464deac2dd
EDFward <edfward@gmail.com>
2015-02-20 17:35:43 -0500

    Automatically Committed for MakeGamesWithUs

--- VocabRPG.spritebuilder/RightBlock.m
+++ VocabRPG.spritebuilder/RightBlock.m
@@ -15,7 +15,7 @@
 - (void)didLoadFromCCB {
   CCLabelBMFont *label =
       [CCLabelBMFont labelWithString:@"junjiah"
-                             fntFile:@"fonts/markerFelt.fnt"
+                             fntFile:@"fonts/helvetica-32.fnt"
                                width:_button.contentSize.width
                            alignment:CCTextAlignmentCenter];
   label.position = ccp(0.5, 0.5);
