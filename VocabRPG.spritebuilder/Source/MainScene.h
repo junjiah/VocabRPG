8476eb60955944ba2419b25a0798b1c5f1898c0a
EDFward <edfward@gmail.com>
2015-02-20 00:38:28 -0500

    Automatically Committed for MakeGamesWithUs

--- VocabRPG.spritebuilder/Source/MainScene.h
+++ VocabRPG.spritebuilder/Source/MainScene.h
@@ -1,7 +1,13 @@
 #import "CCNode.h"
 #import "GamePlayScene.h"
 
-@interface MainScene : GamePlayScene
+@interface MainScene: CCNode <CCPhysicsCollisionDelegate>
+
+@property (nonatomic, strong) Character* character;
+
+@property (nonatomic, strong) CCPhysicsNode* physicsNode;
+
+@property (nonatomic, assign) float timeSinceObstacle;
 
 -(void) gameOver;
 
--- VocabRPG.spritebuilder/Source/MainScene.m
+++ VocabRPG.spritebuilder/Source/MainScene.m
@@ -1,150 +1,150 @@
 #import "MainScene.h"
 #import "Obstacle.h"
 
-@interface CGPointObject : NSObject
-{
-    CGPoint _ratio;
-    CGPoint _offset;
-    CCNode *__unsafe_unretained _child; // weak ref
+@interface CGPointObject : NSObject {
+  CGPoint _ratio;
+  CGPoint _offset;
+  CCNode *__unsafe_unretained _child; // weak ref
 }
-@property (nonatomic,readwrite) CGPoint ratio;
-@property (nonatomic,readwrite) CGPoint offset;
-@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
-+(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
--(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+@property(nonatomic, readwrite) CGPoint ratio;
+@property(nonatomic, readwrite) CGPoint offset;
+@property(nonatomic, readwrite, unsafe_unretained) CCNode *child;
++ (id)pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+- (id)initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
 @end
 
 @implementation MainScene {
-    NSTimeInterval _sinceTouch;
-    
-    CCButton *_restartButton;
-    
-    BOOL _gameOver;
-    CCLabelTTF *_scoreLabel;
-    CCLabelTTF *_nameLabel;
-    
-    int points;
+  NSTimeInterval _sinceTouch;
+
+  CCButton *_restartButton;
+
+  BOOL _gameOver;
+  CCLabelTTF *_nameLabel;
+
+  int points;
 }
 
+- (void)initialize {
+  _character = (Character *)[CCBReader load:@"Character"];
+  [_physicsNode addChild:_character];
+  [self addObstacle];
+  _timeSinceObstacle = 0.0f;
+}
 
 - (void)didLoadFromCCB {
-    self.userInteractionEnabled = TRUE;
-    
-    // set this class as delegate
-    physicsNode.collisionDelegate = self;
-    
-    points = 0;
-    _scoreLabel.visible = true;
-    
-    [super initialize];
+  self.userInteractionEnabled = TRUE;
+
+  // set this class as delegate
+  _physicsNode.collisionDelegate = self;
+
+  points = 0;
+
+  [self initialize];
 }
 
 #pragma mark - Touch Handling
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-    if (!_gameOver) {
-        [character.physicsBody applyAngularImpulse:10000.f];
-        _sinceTouch = 0.f;
-        
-        @try
-        {
-            [super touchBegan:touch withEvent:event];
-        }
-        @catch(NSException* ex)
-        {
-            
-        }
+  if (!_gameOver) {
+    [_character.physicsBody applyAngularImpulse:10000.f];
+    _sinceTouch = 0.f;
+
+    @try {
+      [_character flap];
     }
+    @catch (NSException *ex) {
+    }
+  }
 }
 
 #pragma mark - Game Actions
 
 - (void)gameOver {
-    if (!_gameOver) {
-        _gameOver = TRUE;
-        _restartButton.visible = TRUE;
-        
-        character.physicsBody.velocity = ccp(0.0f, character.physicsBody.velocity.y);
-        character.rotation = 90.f;
-        character.physicsBody.allowsRotation = FALSE;
-        [character stopAllActions];
-        
-        CCActionMoveBy *moveBy = [CCActionMoveBy actionWithDuration:0.2f position:ccp(-2, 2)];
-        CCActionInterval *reverseMovement = [moveBy reverse];
-        CCActionSequence *shakeSequence = [CCActionSequence actionWithArray:@[moveBy, reverseMovement]];
-        CCActionEaseBounce *bounce = [CCActionEaseBounce actionWithAction:shakeSequence];
-        
-        [self runAction:bounce];
-    }
+  if (!_gameOver) {
+    _gameOver = TRUE;
+    _restartButton.visible = TRUE;
+
+    _character.physicsBody.velocity =
+        ccp(0.0f, _character.physicsBody.velocity.y);
+    _character.rotation = 90.f;
+    _character.physicsBody.allowsRotation = FALSE;
+    [_character stopAllActions];
+
+    CCActionMoveBy *moveBy =
+        [CCActionMoveBy actionWithDuration:0.2f position:ccp(-2, 2)];
+    CCActionInterval *reverseMovement = [moveBy reverse];
+    CCActionSequence *shakeSequence =
+        [CCActionSequence actionWithArray:@[ moveBy, reverseMovement ]];
+    CCActionEaseBounce *bounce =
+        [CCActionEaseBounce actionWithAction:shakeSequence];
+
+    [self runAction:bounce];
+  }
 }
 
 - (void)restart {
-    CCScene *scene = [CCBReader loadAsScene:@"MainScene"];
-    [[CCDirector sharedDirector] replaceScene:scene];
+  CCScene *scene = [CCBReader loadAsScene:@"MainScene"];
+  [[CCDirector sharedDirector] replaceScene:scene];
 }
 
 #pragma mark - Obstacle Spawning
 
 - (void)addObstacle {
-    Obstacle *obstacle = (Obstacle *)[CCBReader load:@"Obstacle"];
-    CGPoint screenPosition = [self convertToWorldSpace:ccp(380, 0)];
-    CGPoint worldPosition = [physicsNode convertToNodeSpace:screenPosition];
-    obstacle.position = worldPosition;
-    [obstacle setupRandomPosition];
-    obstacle.zOrder = DrawingOrderPipes;
-    [physicsNode addChild:obstacle];
+  Obstacle *obstacle = (Obstacle *)[CCBReader load:@"Obstacle"];
+  CGPoint screenPosition = [self convertToWorldSpace:ccp(380, 0)];
+  CGPoint worldPosition = [_physicsNode convertToNodeSpace:screenPosition];
+  obstacle.position = worldPosition;
+  [obstacle setupRandomPosition];
+  obstacle.zOrder = DrawingOrderPipes;
+  [_physicsNode addChild:obstacle];
 }
 
 #pragma mark - Update
 
-- (void)showScore
-{
-    _scoreLabel.string = [NSString stringWithFormat:@"%d", points];
-    _scoreLabel.visible = true;
+- (void)update:(CCTime)delta {
+  _sinceTouch += delta;
+
+  _character.rotation = clampf(_character.rotation, -30.f, 90.f);
+
+  if (_character.physicsBody.allowsRotation) {
+    float angularVelocity =
+        clampf(_character.physicsBody.angularVelocity, -2.f, 1.f);
+    _character.physicsBody.angularVelocity = angularVelocity;
+  }
+
+  if ((_sinceTouch > 0.5f)) {
+    [_character.physicsBody applyAngularImpulse:-40000.f * delta];
+  }
+
+  _physicsNode.position =
+      ccp(_physicsNode.position.x - (_character.physicsBody.velocity.x * delta),
+          _physicsNode.position.y);
+
+  if (!_gameOver) {
+    @try {
+      _character.physicsBody.velocity = ccp(
+          80.f, clampf(_character.physicsBody.velocity.y, -MAXFLOAT, 200.f));
+
+      [super update:delta];
+    }
+    @catch (NSException *ex) {
+    }
+  }
 }
 
-- (void)update:(CCTime)delta
-{
-    _sinceTouch += delta;
-    
-    character.rotation = clampf(character.rotation, -30.f, 90.f);
-    
-    if (character.physicsBody.allowsRotation) {
-        float angularVelocity = clampf(character.physicsBody.angularVelocity, -2.f, 1.f);
-        character.physicsBody.angularVelocity = angularVelocity;
-    }
-    
-    if ((_sinceTouch > 0.5f)) {
-        [character.physicsBody applyAngularImpulse:-40000.f*delta];
-    }
-    
-    physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
-  
-    if (!_gameOver)
-    {
-        @try
-        {
-            character.physicsBody.velocity = ccp(80.f, clampf(character.physicsBody.velocity.y, -MAXFLOAT, 200.f));
-            
-            [super update:delta];
-        }
-        @catch(NSException* ex)
-        {
-            
-        }
-    }
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair
+                      character:(CCSprite *)character
+                          level:(CCNode *)level {
+  [self gameOver];
+  return TRUE;
 }
 
--(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair*)pair character:(CCSprite*)character level:(CCNode*)level {
-    [self gameOver];
-    return TRUE;
-}
-
--(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair character:(CCNode *)character goal:(CCNode *)goal {
-    [goal removeFromParent];
-    points++;
-    _scoreLabel.string = [NSString stringWithFormat:@"%d", points];
-    return TRUE;
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair
+                      character:(CCNode *)character
+                           goal:(CCNode *)goal {
+  [goal removeFromParent];
+  points++;
+  return TRUE;
 }
 
 @end
