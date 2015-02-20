8792f30507be339299c760d326d80794370f2c79
EDFward <edfward@gmail.com>
2015-02-20 17:55:58 -0500

    Automatically Committed for MakeGamesWithUs

--- VocabRPG.spritebuilder/RightBlock.m
+++ VocabRPG.spritebuilder/RightBlock.m
@@ -13,12 +13,12 @@
 }
 
 - (void)didLoadFromCCB {
-//  CCLabelBMFont *label =
-//      [CCLabelBMFont labelWithString:@"junjiah"
-//                             fntFile:@"Chalkduster.ttf"
-//                               width:_button.contentSize.width
-//                           alignment:CCTextAlignmentCenter];
-  CCLabelTTF *label = [CCLabelTTF labelWithString:@"junjiah" fontName:@"Chalkduster.ttf" fontSize:14.0f];
+  CCLabelBMFont *label =
+      [CCLabelBMFont labelWithString:@"junjiah"
+                             fntFile:@"Chalkduster.ttf"
+                               width:_button.contentSize.width
+                           alignment:CCTextAlignmentCenter];
+//  CCLabelTTF *label = [CCLabelTTF labelWithString:@"junjiah" fontName:@"Chalkduster.ttf" fontSize:14.0f];
   label.position = ccp(0.5, 0.5);
   label.positionType = CCPositionTypeNormalized;
   [_button addChild:label];
