e2434d0161ee0266b13b95a33462ef6a73627e82
EDFward <edfward@gmail.com>
2015-02-20 17:53:24 -0500

    Automatically Committed for MakeGamesWithUs

--- VocabRPG.spritebuilder/LeftBlock.m
+++ VocabRPG.spritebuilder/LeftBlock.m
@@ -13,11 +13,12 @@
 }
 
 - (void)didLoadFromCCB {
-  CCLabelBMFont *label =
-      [CCLabelBMFont labelWithString:@"junjiah"
-                             fntFile:@"Chalkduster.ttf"
-                               width:_button.contentSize.width
-                           alignment:CCTextAlignmentCenter];
+//  CCLabelBMFont *label =
+//      [CCLabelBMFont labelWithString:@"junjiah"
+//                             fntFile:@"Chalkduster.ttf"
+//                               width:_button.contentSize.width
+//                           alignment:CCTextAlignmentCenter];
+  CCLabelTTF *label = [CCLabelTTF labelWithAttributedString:@"junjiah"];
   label.position = ccp(0.5, 0.5);
   label.positionType = CCPositionTypeNormalized;
   [_button addChild:label];
