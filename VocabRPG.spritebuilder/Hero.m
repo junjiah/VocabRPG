e537da7a83f7a2deca99e7110c66f882bdf5ff87
EDFward <edfward@gmail.com>
2015-02-20 16:00:37 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ VocabRPG.spritebuilder/Hero.m
@@ -0,0 +1,13 @@
+//
+//  Hero.m
+//  VocabRPG
+//
+//  Created by Junjia He on 2/20/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Hero.h"
+
+@implementation Hero
+
+@end
