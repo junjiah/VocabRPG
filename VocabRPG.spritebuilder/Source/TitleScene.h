f1c092afcef0b0316ad60d258c03fbdba0c30207
EDFward <edfward@gmail.com>
2015-04-14 15:47:17 -0400

    added title screen and its class

---
+++ VocabRPG.spritebuilder/Source/TitleScene.h
@@ -0,0 +1,13 @@
+//
+//  TitleScene.h
+//  VocabRPG
+//
+//  Created by Junjia He on 4/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface TitleScene : CCScene
+
+@end
---
+++ VocabRPG.spritebuilder/Source/TitleScene.m
@@ -0,0 +1,13 @@
+//
+//  TitleScene.m
+//  VocabRPG
+//
+//  Created by Junjia He on 4/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "TitleScene.h"
+
+@implementation TitleScene
+
+@end
